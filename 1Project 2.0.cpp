ba25c811e515f7598e07c37159053cc21e1c7394
Dvitri <118054844+Gladiaator321@users.noreply.github.com>
2024-11-25 20:50:49 +0200

    Update 1Project 2.0.cpp

--- 1Project 2.0.cpp
+++ 1Project 2.0.cpp
@@ -5,13 +5,12 @@ int main() {
 	int UserNumber, RandomNumber, attempts = 10; //attempts (clasic 10)
 	srand(time(0));
 	RandomNumber = rand() % 100;				//random number generator in range[0;100]
-	cout << "Enter your number (â³ä 0-100): ";cin >> UserNumber;
 	while (attempts >= 1) {
+		cout << "Number of attempts, that left: " << attempts << endl << "Enter your number (від 0-100): ";cin >> UserNumber;   //attempts of guessing
 		if (UserNumber == RandomNumber) {
 			cout << "You won!!!" << endl;break;						//if you won
 		}
 		if (UserNumber > RandomNumber) cout << "Number is smaller." << endl;else cout << "Number is bigger." << endl;		//number bigger or smaller than yours
-		cout << "Number of attempts, that left: " << attempts << endl << "Enter your number (â³ä 0-100): ";cin >> UserNumber;   //attempts of guessing
 		attempts--;
 	}
 	if (attempts < 1) cout << "Game over! :( Number was: " << RandomNumber << endl;  //if your attempts are over
