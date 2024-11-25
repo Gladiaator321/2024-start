2e1968d785f1fd60db7ea3698091bc9f856f5660
Dvitri <118054844+Gladiaator321@users.noreply.github.com>
2024-11-25 20:36:10 +0200

    1Project 2.0.cpp

--- 1Project 2.0.cpp
+++ 1Project 2.0.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <ctime>
-#include <locale>
-#include "Windows.h"
 using namespace std;
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
 	int UserNumber, RandomNumber, attempts = 10; //attempts (clasic 10)
 	srand(time(0));
 	RandomNumber = rand() % 100;				//random number generator in range[0;100]
-	cout << "Enter your number (â³ä 0-100): ";cin >> UserNumber;
+	cout << "Enter your number (Ã¢Â³Ã¤ 0-100): ";cin >> UserNumber;
 	while (attempts >= 1) {
 		if (UserNumber == RandomNumber) {
 			cout << "You won!!!" << endl;break;						//if you won
 		}
 		if (UserNumber > RandomNumber) cout << "Number is smaller." << endl;else cout << "Number is bigger." << endl;		//number bigger or smaller than yours
-		cout << "Number of attempts, that left: " << attempts << endl << "Enter your number (â³ä 0-100): ";cin >> UserNumber;   //attempts of guessing
+		cout << "Number of attempts, that left: " << attempts << endl << "Enter your number (Ã¢Â³Ã¤ 0-100): ";cin >> UserNumber;   //attempts of guessing
 		attempts--;
 	}
 	if (attempts < 1) cout << "Game over! :( Number was: " << RandomNumber << endl;  //if your attempts are over
